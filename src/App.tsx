6af3913a20705d0fd0374d987a6e44c77d112c72
andrewberryca <andrew.berry.ca@gmail.com>
2022-07-18 00:28:30 +0200

    base setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import {HomePage} from "./pages/HomePage";
+import {FavouritePage} from "./pages/FavouritePage";
+import {Navigation} from "./components/Navigation";
 
 function App() {
   return (
-    <h1 className="font-bold">Hey React!</h1>
-  );
+      <>
+          <Navigation />
+          <Routes>
+              <Route path = "/" element={<HomePage />}/>
+              <Route path = "/favourite" element={ <FavouritePage/>}/>
+          </Routes>
+
+      </>
+
+     )
 }
 
 export default App;
