da7076d88dbcae891f88f226ee25d5b0dbc3c362
andrewberryca <andrew.berry.ca@gmail.com>
2022-07-18 12:26:06 +0200

    updated response with limit to 10

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,8 @@ import {useSearchUsersQuery} from "../store/github/github.api";
 
 export function HomePage() {
     const {isLoading, isError, data} = useSearchUsersQuery('andrewberryca')
+
+    console.log(data)
     return (
         <div>Home</div>
     )
