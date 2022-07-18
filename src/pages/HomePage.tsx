7f5a4a5eba48af0cba7e5c035c0b786c6f19e5a6
andrewberryca <andrew.berry.ca@gmail.com>
2022-07-18 22:10:13 +0200

    added dropdown search list

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useSearchUsersQuery} from "../store/github/github.api";
+import {useDebounce} from "../hooks/debounce";
 
 export function HomePage() {
-    const {isLoading, isError, data} = useSearchUsersQuery('andrewberryca')
+    const [search, setSearch] = useState('')
+    const [dropdown, setDropdown] = useState(false)
+    const debounced = useDebounce(search)
+    const {isLoading, isError, data} = useSearchUsersQuery(debounced, {
+        skip: debounced.length < 4 /** The minimum number of symbols in GitHub username **/
+    })
 
-    console.log(data)
+useEffect(() => {
+    setDropdown(debounced.length > 3 && data?.length! > 0)
+}, [debounced, data])
+
     return (
-        <div>Home</div>
+        <div className="flex justify-center pt-5 mx-auto h-screen w-screen">
+            {isError && <p className="text-center text-red-700">Something isn't right..</p>}
+            <div className="relative w-[600px]">
+                <input
+                type="text"
+                className="border py-4 px-4 w-full h-[50px] mb-4"
+                placeholder=".. Search GitHub by User name"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+                />
+
+                {dropdown &&  <ul className="absolute top-[50px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
+                    { isLoading && <p className="text-center">Loading..</p> }
+                    { data?.map(user => (
+                        <li
+                        key={user.id}
+                        className="py-2 px-4 hover:text-white transition-colors cursor-pointer"
+
+                        >{user.login}</li>
+                    ))}
+                </ul>}
+            </div>
+        </div>
     )
 }
